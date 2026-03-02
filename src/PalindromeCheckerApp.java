f0b94f424505fa011475f7e5ce88b9d25e82c5ec
ay1786-ctrl <ay1786@srmist.edu.in>
2026-03-02 14:36:28 +0530

    UC8: Linked List based palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,4 +1,4 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+git add src/PalindromeCheckerApp.java//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
   //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
